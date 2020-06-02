88001860830d806554aea22927115226e4eaf196
harjaapdhillon <harvydhillon16@gmail.com>
2020-06-02 17:33:57 +0530

    footer made

---
+++ src/components/Footer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  img {
+    max-height: 2rem;
+  }
+  .subtitle {
+    margin-top: 10px;
+  }
+  .image {
+    min-height: 2rem;
+    min-width: 2rem;
+  }
+`;
+const SocialData = [
+  {
+    uri:
+      'https://sguru.org/wp-content/uploads/2018/02/5a2fe479cc45e43754640849.png',
+  },
+  {
+    uri:
+      'https://cdn.freebiesupply.com/logos/large/2x/facebook-logo-black-2019.png',
+  },
+  {
+    uri:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Instagram_simple_icon.svg/1024px-Instagram_simple_icon.svg.png',
+  },
+];
+const PageLinks = [
+  { page: 'About' },
+  { page: 'Faq' },
+  { page: 'Contact' },
+  { page: 'Blog' },
+];
+
+const Footer = () => {
+  return (
+    <Container className=" has-background-light">
+      <div className="section">
+        <div className="container">
+          <div className="columns is-multiline">
+            <div className="column is-2">
+              <img src="images/logo.svg" alt="logo" />
+              <h1 className="subtitle is-6">© 2019 Company</h1>
+            </div>
+            <div className="column is-6" />
+            <div className="column">
+              <div className="columns is-multiline has-text-centered">
+                <div className="column is-6" />
+                {SocialData.map(data => (
+                  <div className="column is-2">
+                    <img className="image" src={data.uri} alt="logo" />
+                  </div>
+                ))}
+                {PageLinks.map(data => (
+                  <div className="column is-3">
+                    <h1 className="subtitle is-6">{data.page}</h1>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+export default Footer;
