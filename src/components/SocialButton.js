c98b777c031864602771d501977e1f64f2c12c05
harjaapdhillon <harvydhillon16@gmail.com>
2020-06-03 16:39:44 +0530

    Minor Fix

--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  width: 100%;
-  span {
-    align-self: center;
-  }
-`;
-
 const SocialButton = () => {
   return (
-    <Container
-      type="button"
-      className=" columns button is-vcentered is-centered"
-    >
-      <div className="column is-1">
-        <span className="icon is-medium ">
-          <i className="fab fa-github" />
-        </span>
-      </div>
-      <div className="column is-11 ">
-        <span>GitHub</span>
-      </div>
-    </Container>
+    <button type="button" className="button is-medium ">
+      <span className="icon ">
+        <i className="fab fa-github has-icons-left" />
+      </span>
+      <span>GitHub</span>
+    </button>
   );
 };
 export default SocialButton;
